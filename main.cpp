c704a649f30be706d8cd5259b77768c9fa1a51fa
aizwellenstan <aizwellenstan@gmail.com>
2023-12-10 12:37:33 +0900

    update strike

--- main.cpp
+++ main.cpp
@@ -15,52 +15,13 @@ struct OptionData {
 };
 
 int main() {
-    // std::ifstream file("data/option/qqq/qqq_eod_202103.txt");
-    // if (!file.is_open()) {
-    //     std::cerr << "Error opening file." << std::endl;
-    //     return 1;
-    // }
-
-    // // Skip the header line
-    // std::string header;
-    // std::getline(file, header);
-
-    // std::map<std::pair<std::string, int>, OptionData> dataMap;
-
-    // std::string line;
-    // while (std::getline(file, line)) {
-    //     std::istringstream iss(line);
-    //     std::string token;
-
-    //     // Read values from CSV line
-    //     std::vector<std::string> v;
-    //     while (std::getline(iss, token, ',')) {
-    //         v.push_back(token);
-    //     }
-
-    //     if (v.size() >= 32) { // Assuming there are at least 32 columns
-    //         OptionData optionData;
-    //         optionData.quoteDate = trim(v[2]); // QUOTE_DATE
-    //         optionData.dte = std::stoi(v[7]); // DTE
-    //         optionData.cGamma = std::stod(v[9]); // C_GAMMA
-    //         optionData.pGamma = std::stod(v[25]); // P_GAMMA
-    //         optionData.strike = std::stod(v[19]); // STRIKE
-
-    //         // Update the map if a higher value is found
-    //         auto key = std::make_pair(optionData.quoteDate, optionData.dte);
-    //         if (dataMap.find(key) == dataMap.end() || dataMap[key].cGamma + dataMap[key].pGamma < optionData.cGamma + optionData.pGamma) {
-    //             dataMap[key] = optionData;
-    //         }
-    //     }
-    // }
-
-    // file.close();
+    // ... (Existing code remains unchanged)
 
     std::string folderPath = "data/option/qqq";
     std::vector<std::string> allFiles = listFilesInDirectory(folderPath);
     std::vector<std::vector<std::string>> ct;  
     read_csv_from_file_list(ct, allFiles);
-    std::map<std::pair<std::string, int>, OptionData> dataMap;
+    std::map<std::tuple<std::string, int, double>, OptionData> dataMap;  // Use std::tuple as the key type
     for (const auto& v : ct) {
         OptionData optionData;
         optionData.quoteDate = v[2]; // QUOTE_DATE
@@ -70,10 +31,8 @@ int main() {
         optionData.strike = std::stod(v[19]); // STRIKE
 
         // Update the map if a higher value is found
-        auto key = std::make_pair(optionData.quoteDate, optionData.dte);
-        if (dataMap.find(key) == dataMap.end() || dataMap[key].cGamma + dataMap[key].pGamma < optionData.cGamma + optionData.pGamma) {
-            dataMap[key] = optionData;
-        }
+        auto key = std::make_tuple(optionData.quoteDate, optionData.dte, optionData.strike);
+        dataMap[key] = optionData;
     }
 
     // Write the highest C_GAMMA + P_GAMMA values and corresponding strike to CSV
@@ -84,14 +43,14 @@ int main() {
     }
 
     // Write CSV header
-    outputFile << "QUOTE_DATE,DTE,C_GAMMA_P_GAMMA,STRIKE\n";
+    outputFile << "DATE,DTE,C_GAMMA_P_GAMMA,STRIKE\n";
 
     // Write data to CSV
     for (const auto& entry : dataMap) {
-        outputFile << entry.second.quoteDate << ","
-                   << entry.second.dte << ","
+        outputFile << std::get<0>(entry.first) << ","
+                   << std::get<1>(entry.first) << ","
                    << entry.second.cGamma + entry.second.pGamma << ","
-                   << entry.second.strike << "\n";
+                   << std::get<2>(entry.first) << "\n";
     }
 
     outputFile.close();
